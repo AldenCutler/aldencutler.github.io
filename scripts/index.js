1ae71b6f6e546a91037d8f644e3d66f00e91ec0d
Alden Cutler <aldencutler84@gmail.com>
2024-09-15 13:36:38 -0400

    update website to be relevant again

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,30 @@
 console.clear();
 
-const burger = document.getElementById('burger');
-const progressBars = document.querySelectorAll('.progress');
+const burger = document.getElementById("burger");
+const progressBars = document.querySelectorAll(".progress");
 const values = {
-    'advanced': 80,
-    'intermediate/advanced': 70,
-    'intermediate': 60,
-    'beginner/intermediate': 50,
-    'beginner': 40,
-}
+  expert: 100,
+  advanced: 90,
+  "intermediate/advanced": 80,
+  intermediate: 70,
+  "beginner/intermediate": 60,
+  beginner: 50,
+};
 
-window.mobileCheck = function() {
-    let check = false;
-    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-    return check;
+window.mobileCheck = function () {
+  let check = false;
+  (function (a) {
+    if (
+      /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+        a
+      ) ||
+      /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+        a.substr(0, 4)
+      )
+    )
+      check = true;
+  })(navigator.userAgent || navigator.vendor || window.opera);
+  return check;
 };
 
 /**
@@ -21,172 +32,165 @@ window.mobileCheck = function() {
  * Change color of progress bar based on value
  */
 progressBars.forEach((p) => {
-    let value = p.innerText;
-    p.innerText = '';
-    // console.log(value);
-    p.style.width = `${values[value]}%`;
+  let value = p.innerText;
+  p.innerText = "";
+  // console.log(value);
+  p.style.width = `${values[value]}%`;
 
-    // change color of progress bar
-    if (values[value] == 80) {
-        p.style.backgroundColor = '#00ff00';
-    } else if (values[value] == 70) {
-        p.style.backgroundColor = '#bbff00';
-    } else if (values[value] == 60) {
-        p.style.backgroundColor = '#bbdd00';
-    } else if (values[value] == 50) {
-        p.style.backgroundColor = '#bbbb00';
-    } else if (values[value] == 40) {
-        p.style.backgroundColor = '#ff8800';
-    }
+  // change color of progress bar
+  if (values[value] == 100) {
+    p.style.backgroundColor = "#00ff00";
+  } else if (values[value] == 90) {
+    p.style.backgroundColor = "#6bee00";
+  } else if (values[value] == 80) {
+    p.style.backgroundColor = "#95db00";
+  } else if (values[value] == 70) {
+    p.style.backgroundColor = "#b0c800";
+  } else if (values[value] == 60) {
+    p.style.backgroundColor = "#c5b400";
+  } else if (values[value] == 50) {
+    p.style.backgroundColor = "#d79f00";
+  }
 });
 
 /**
  * If mobile, remove nav bar and replace with burger
  * If desktop, remove burger and center content
-*/
+ */
 if (mobileCheck()) {
-    // remove nav bar and replace with burger
-    document.querySelector('.nav-links').style.display = 'none';
-    burger.style.display = 'block';
+  // remove nav bar and replace with burger
+  document.querySelector(".nav-links").style.display = "none";
+  burger.style.display = "block";
 
-    // add event listener to burger for click
-    burger.addEventListener('click', () => {
+  // add event listener to burger for click
+  burger.addEventListener("click", () => {});
 
-    });
-
-    const separators = document.querySelectorAll('.sep');
-    separators.forEach((s) => {
-        s.style.display = 'none';
-    });
-
+  const separators = document.querySelectorAll(".sep");
+  separators.forEach((s) => {
+    s.style.display = "none";
+  });
 } else {
-    document.getElementById('burger').remove();
+  document.getElementById("burger").remove();
 
-    // document.body.style.lineHeight = '1.5rem';
+  // document.body.style.lineHeight = '1.5rem';
 
-    // // center nav bar
-    const nav = document.querySelector('.nav-links');
-    nav.style.color = '#fff';
-    // nav.style.margin = 'auto';
-    // nav.style.padding = '1rem 0';
+  // // center nav bar
+  const nav = document.querySelector(".nav-links");
+  nav.style.color = "#fff";
+  // nav.style.margin = 'auto';
+  // nav.style.padding = '1rem 0';
 
-    // center header and resize font
-    const header = document.querySelector('.header');
-    header.style.marginTop = '5rem';
-    header.style.textAlign = 'center';
-    document.querySelector('.name').style.fontSize = '3rem';
-    document.querySelector('.tagline').style.fontSize = '1.5rem';
+  // center header and resize font
+  const header = document.querySelector(".header");
+  header.style.marginTop = "5rem";
+  header.style.textAlign = "center";
+  document.querySelector(".name").style.fontSize = "3rem";
+  document.querySelector(".tagline").style.fontSize = "1.5rem";
 
-    // center about me section
-    const about = document.querySelector('.about-me');
-    about.style.textAlign = 'center';
-    about.style.width = '70%';
-    about.style.margin = 'auto';
-    about.style.paddingTop = '3rem';
+  // center about me section
+  const about = document.querySelector(".about-me");
+  about.style.textAlign = "center";
+  about.style.width = "70%";
+  about.style.margin = "auto";
+  about.style.paddingTop = "3rem";
 
-    const socials = document.getElementById('socials');
-    socials.style.width = '25%';
-    socials.style.marginBottom = '1rem';
+  const socials = document.getElementById("socials");
+  socials.style.width = "25%";
+  socials.style.marginBottom = "1rem";
 
-    const text = document.querySelectorAll('.about-text');
-    text.forEach((t) => {
-        t.style.width = '50%';
-        t.style.margin = 'auto';
-        t.style.padding = '1rem 0';
-        t.style.lineHeight = '1.5rem';
-    });
+  const text = document.querySelectorAll(".about-text");
+  text.forEach((t) => {
+    t.style.width = "50%";
+    t.style.margin = "auto";
+    t.style.padding = "1rem 0";
+    t.style.lineHeight = "1.5rem";
+  });
 
-    // center skills section
-    const skills = document.querySelector('.skills');
-    skills.style.textAlign = 'center';
-    skills.style.width = '50%';
-    skills.style.margin = 'auto';
-    skills.style.padding = '1rem 0';
-    skills.style.paddingTop = '3rem';
+  // center skills section
+  const skills = document.querySelector(".skills");
+  skills.style.textAlign = "center";
+  skills.style.width = "50%";
+  skills.style.margin = "auto";
+  skills.style.padding = "1rem 0";
+  skills.style.paddingTop = "3rem";
 
-    const projects = document.querySelector('.projects');
-    projects.style.width = '75%';
-    projects.style.margin = 'auto';
-    projects.style.textAlign = 'center';
-    projects.style.paddingTop = '3rem';
-    projects.style.paddingBottom = '3rem';
+  const projects = document.querySelector(".projects");
+  projects.style.width = "60%";
+  projects.style.margin = "auto";
+  projects.style.textAlign = "center";
+  projects.style.paddingTop = "3rem";
+  projects.style.paddingBottom = "3rem";
 
-    const education = document.querySelector('.education');
-    education.style.width = '75%';
-    education.style.margin = 'auto';
-    education.style.paddingTop = '3rem';
-    education.style.paddingBottom = '3rem';
+  const education = document.querySelector(".education");
+  education.style.width = "60%";
+  education.style.margin = "auto";
+  education.style.paddingTop = "3rem";
+  education.style.paddingBottom = "3rem";
 
-    const awards = document.querySelector('.awards');
-    awards.style.width = '75%';
-    awards.style.margin = 'auto';
-    awards.style.paddingTop = '3rem';
-    awards.style.paddingBottom = '3rem';
+  const awards = document.querySelector(".awards");
+  awards.style.width = "60%";
+  awards.style.margin = "auto";
+  awards.style.paddingTop = "3rem";
+  awards.style.paddingBottom = "3rem";
 
-    const experience = document.querySelector('.experience');
-    experience.style.width = '75%';
-    experience.style.margin = 'auto';
-    experience.style.paddingTop = '3rem';
-    experience.style.paddingBottom = '3rem';
+  const experience = document.querySelector(".experience");
+  experience.style.width = "60%";
+  experience.style.margin = "auto";
+  experience.style.paddingTop = "3rem";
+  experience.style.paddingBottom = "3rem";
 
-    const grid = document.querySelectorAll('.grid');
-    grid.forEach((g) => {
-        g.style.display = 'grid';
-        g.style.gridTemplateColumns = 'repeat(2, 1fr)';
-        g.style.gridGap = '1rem';
-        // g.style.width = '50%';
-        g.style.margin = 'auto';
-        g.style.padding = '1rem 0';
-    });
+  const grid = document.querySelectorAll(".grid");
+  grid.forEach((g) => {
+    g.style.display = "grid";
+    g.style.gridTemplateColumns = "repeat(2, 1fr)";
+    g.style.gridGap = "1rem";
+    // g.style.width = '50%';
+    g.style.margin = "auto";
+    g.style.padding = "1rem 0";
+  });
 
-    const sectionHeaders = document.querySelectorAll('.section-header');
-    sectionHeaders.forEach((s) => {
-        s.style.width = '50%';
-        s.style.margin = 'auto';
-        s.style.padding = '1rem 0';
-        s.style.textAlign = 'center';
-    });
+  const sectionHeaders = document.querySelectorAll(".section-header");
+  sectionHeaders.forEach((s) => {
+    s.style.width = "50%";
+    s.style.margin = "auto";
+    s.style.padding = "1rem 0";
+    s.style.textAlign = "center";
+  });
 
-    document.querySelector('.school').style.marginTop = '0';   
-
-    document.querySelector('.contact-me').style.textAlign = 'center';
-
+  document.querySelector(".school").style.marginTop = "0";
 }
+
 if (mobileCheck()) {
-    // Burger Menu
-    const nav = document.querySelector('.nav-links');
-    const links = document.querySelectorAll('.nav-links li');
-    burger.addEventListener('click', () => {
-        
-        if (nav.style.display === 'none') {
-            nav.style.display = 'block';
-            nav.style.color = '#fff';
+  // Burger Menu
+  const nav = document.querySelector(".nav-links");
+  const links = document.querySelectorAll(".nav-links li");
+  burger.addEventListener("click", () => {
+    if (nav.style.display === "none") {
+      nav.style.display = "block";
+      nav.style.color = "#fff";
 
-            // add 10px margin to top of each link except first
-            links.forEach((link) => {
-                link.style.marginTop = '10px';
-            });
-            links[0].style.marginTop = '0';
+      // add 10px margin to top of each link except first
+      links.forEach((link) => {
+        link.style.marginTop = "10px";
+      });
+      links[0].style.marginTop = "0";
+    } else {
+      nav.style.display = "none";
+    }
+  });
 
-        }
-        else {
-            nav.style.display = 'none';
-        }
-    
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      nav.style.display = "none";
     });
-    
-    links.forEach((link) => {
-        link.addEventListener('click', () => {
-            nav.style.display = 'none';
-        });
-    });
-    
-    // close nav bar when clicking outside of it
-    window.addEventListener('click', (e) => {
-        if (e.target != burger && e.target.parentNode != burger) {
-            nav.style.display = 'none';
-        }
-    });
+  });
+
+  // close nav bar when clicking outside of it
+  window.addEventListener("click", (e) => {
+    if (e.target != burger && e.target.parentNode != burger) {
+      nav.style.display = "none";
+    }
+  });
 }
 
-const tags = document.querySelectorAll('.tag');
+const tags = document.querySelectorAll(".tag");
